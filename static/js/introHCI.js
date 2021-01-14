36ab101fc07b1b96d5e2281dcff5dc65474aa9e6
HermesKeng <kaining1101@gmail.com>
2021-01-14 10:46:15 -0800

    lab2 fix something

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
   initializePage();
 });
+
+function projectClick(e) {
+  e.preventDefault();
+  $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
   $("#testjs").click(function (e) {
-    alert("Javascript is connected");
-    $(".jumbotron h1").text("Javascript is connected");
+    var h1Element = document.getElementById("change");
+    h1Element.innerHTML = "Javascript is connected";
   });
+
+  $("a.thumbnail").click(projectClick);
 }
