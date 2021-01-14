1888e071c9a57be06bd6065399060cd76ad166a5
HermesKeng <kaining1101@gmail.com>
2021-01-14 09:46:40 -0800

    lab2

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    initializePage();
+$(document).ready(function () {
+  initializePage();
 });
-
 function initializePage() {
-    $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+  $("#testjs").click(function (e) {
+    alert("Javascript is connected");
+    $(".jumbotron h1").text("Javascript is connected");
+  });
 }
